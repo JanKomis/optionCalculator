26c3d9eba8f0035bfb76fbcb66c23700325e5f40
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2024-04-24 13:54:00 +0200

    add css to NavBar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,4 +1,6 @@
+import { Heading1 } from "lucide-react";
 import Link from "next/link";
+import React from "react";
 
 const links = [
   {
@@ -15,15 +17,38 @@ const links = [
   },
 ];
 
+const login = true;
+
 export default function NavBar() {
   return (
-    <div className="flex bg-blue-500">
-      {links.map((link) => (
-        <Link href={link.path}>{link.name}</Link>
-      ))}
-      <Link href={"/login"}>Log in</Link>
-      <div>User</div>
+    <div
+      className={`flex h-16 items-center px-4 ${
+        login ? "justify-between" : "justify-end"
+      }`}
+    >
+      {login ? (
+        <>
+          <nav className="space-x-4">
+            {links.map((link) => (
+              <Link
+                key={link.name}
+                href={link.path}
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </nav>
+          <div>User</div>
+        </>
+      ) : (
+        <Link
+          href={"/login"}
+          className="text-sm font-medium transition-colors hover:text-primary"
+        >
+          Log in
+        </Link>
+      )}
     </div>
   );
 }
-//{links.map((link) =>(<Link href={link.path}>{link.name}</Link>))}
