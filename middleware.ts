5e0feb54606061135280400237d6d641c7dce126
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2024-05-06 10:54:47 +0200

    functional middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,28 @@
-//import { auth } from "./auth";
+import { NextResponse } from "next/server";
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
-
-//export const { auth: middleware } = NextAuth(authConfig)
 const { auth } = NextAuth(authConfig);
+
+const privateRoute = ["/strategies", "/calculator", "/plgraph"];
+const authRoute = ["/signin"];
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
-  console.log("Route:", req.nextUrl.pathname);
-  console.log("Logged:", isLoggedIn);
+  const nextPath = req.nextUrl;
+
+  const isAuthRoute = authRoute.includes(nextPath.pathname);
+  const isPrivateRoute = privateRoute.includes(nextPath.pathname);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/strategies", nextPath));
+    }
+    null;
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    return NextResponse.redirect(new URL("/signin", nextPath));
+  }
 });
 
 export const config = {
