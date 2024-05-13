1447d074f210038b9af513d2b2bfac8082abf43d
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2024-05-13 17:30:05 +0200

    add dynamic routing page strategySlug

---
+++ app/strategies/[strategySlug]/page.js
@@ -0,0 +1,17 @@
+export default function Strategy() {
+  return (
+    <>
+      <header>
+        <h1>Ahoj</h1>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+          modi ducimus tempore quasi quo vero nam quidem dolores, beatae
+          tempora, soluta, necessitatibus obcaecati temporibus dolorem maiores
+          molestiae libero autem iusto?
+        </p>
+      </main>
+    </>
+  );
+}
--- components/strategies/DataTable.tsx
+++ components/strategies/DataTable.tsx
@@ -15,6 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "../ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
