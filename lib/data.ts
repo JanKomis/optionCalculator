6e455f15f63f8141ccd806bc1b679fdf52c13395
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2024-05-13 12:51:07 +0200

    add function to get strategy by id

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-import prisma from './db'
+import prisma from "./db";
 
 export async function getAllStrategies() {
   try {
@@ -8,9 +8,22 @@ export async function getAllStrategies() {
         closedPositions: true,
       },
     });
-    console.log(strategies);
     return strategies;
   } catch (error) {
     console.error("Error fetching strategies:", error);
+  }
+}
+
+export async function getStrategyById(strategyId) {
+  try {
+    const strategy = await prisma.strategy.findUnique({
+      where: {
+        id: strategyId,
+      },
+    });
+
+    return strategy;
+  } catch (error) {
+    console.error("Error retrieving strategy: ", error);
   }
 }
